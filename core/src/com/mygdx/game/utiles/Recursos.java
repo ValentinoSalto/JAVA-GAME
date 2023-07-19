00cfe75cc4bcbf682fda53368fb578ef52c121ac
ValentinoSalto <saltovalentino@gmail.com>
2023-07-19 20:54:12 -0300

    Cambio fondo de menu

--- core/src/com/mygdx/game/utiles/Recursos.java
+++ core/src/com/mygdx/game/utiles/Recursos.java
@@ -3,6 +3,6 @@ package com.mygdx.game.utiles;
 public class Recursos {
 	
 	public static final String PANTALLACARGA = "Pantalla de carga/Pantallacarga (1).jpg";
-	public static final String FONDOMENU = "Menu/js1 preview.jpg";
+	public static final String FONDOMENU = "Menu/Fondomenu2.jpg";
 	
 }
